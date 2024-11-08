bf69a7ec86c6f163bb41fb4fb041e9790efafd72
Daniel García Angulo <>
2024-11-08 17:20:05 +0100

    Create lineplot

--- Script/main_script.py
+++ Script/main_script.py
@@ -4,5 +4,16 @@ import pandas as pd
 print('Dani')
 print('Dani')
 print('Dani')
-print('Dani')
 
+# Create lineplot
+print('Dani')
+print('Dani')
+print('Dani')
+print('Dani')
+print('Dani')
+print('Dani')
+print('Dani')
+print('Dani')
+print('Dani')
+print('Dani')
+print('Dani')
